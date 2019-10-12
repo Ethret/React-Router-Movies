2b622a3b07cc4fa2db9a0bffdd3ff3cf04faf737
Ethret <Ethret@users.noreply.github.com>
2019-10-12 00:30:23 -0500

    initial commit, unfinished

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
 
+import MovieList from './Movies/MovieList';
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -9,10 +12,23 @@ const App = () => {
     setSavedList( [...savedList, movie] );
   };
 
+  const [id, setId] = useState();
+
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+      <Route
+        path="/"
+        render={ (props) => {
+          return <MovieList id={id} {...props}/>
+        }}
+      />
+      <Route
+        path ="/movies/:id"
+        render={ props => <Movie id={id} {...props} />}
+      />
+      </div>
     </div>
   );
 };
